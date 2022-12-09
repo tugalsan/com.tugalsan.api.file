b66cb194b684fd56377a8f85aeb96e1288e24a0b
tugalsan karabacak <tugalsan@gmail.com>
2022-12-10 02:20:50 +0300

    watch added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.tugalsan.api.file {
     requires com.tugalsan.api.compiler;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.pack;
+    requires com.tugalsan.api.thread;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.os;
     requires com.tugalsan.api.stream;
